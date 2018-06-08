14d5b5fcb9f231f4ca67d5bbf1d04e42a78c2a48
Luis <luis@gusacapital.com>
2018-06-08 11:25:05 -0500

    Archivos necesarios para API

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: '6b52ade6490ca849b9a18b48346625b3',
+    sharedSecret: 'aa3717fd01a3d3157d07dc369d73a791',
+    registeredTo: 'luisbits'
+}
+
+//Application name platzimusic
+//API key 6b52ade6490ca849b9a18b48346625b3
+//Shared secret aa3717fd01a3d3157d07dc369d73a791
+//Registered to luisbits
